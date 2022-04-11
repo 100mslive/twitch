7a706a730c929efd01e34cab6ca1dd79800759a4
sandeep <sandeep@100ms.live>
2022-04-12 00:12:25 +0530

    added env changes

--- src/utils/getToken.js
+++ src/utils/getToken.js
@@ -1,14 +1,12 @@
-
-const endPoint =
-"https://prod-in.100ms.live/hmsapi/spacesclone.app.100ms.live/";
-
+const endPoint = process.env.REACT_APP_TOKEN_ENDPOINT;
+const room_id = process.env.REACT_APP_ROOM_ID;
 export default async function getToken( role) {
     const response = await fetch(`${endPoint}api/token`, {
       method: 'POST',
       body: JSON.stringify({
         user_id: '5fc62c5872909272bf9995e1', // User ID assigned by you (different from 100ms' assigned id)
         role: role, // listener , speaker , moderator
-        room_id: "61eedaa5d85bf05ee4e1767f"
+        room_id: room_id
       }),
     });
   
